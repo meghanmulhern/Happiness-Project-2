92faa9494294b32433f58393eecc4a56fc702689
Susan Chambers <chamberssu@gmail.com>
2021-04-19 22:11:32 -0500

    Importing CORS and Pandas, removing SQLAlchemy

---
+++ GeoChart/geochart.js
@@ -0,0 +1,20 @@
+google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyDHdelbQAEdfZwnzbCWzu3nQaW2ZO2pBrI'
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap);
+
+  function drawRegionsMap() {
+    d3.json("http://127.0.0.1:5000//api/v1.0/happiness").then(function(happiness_data){
+      var happy_year = happiness_data.filter(x => x.year == 2020).map(x => {
+        return [x.country, x.happiness_score]
+      })
+      happy_year.unshift(["country", "happiness_score"])
+      var data = google.visualization.arrayToDataTable(happy_year)
+      var options = {};
+      var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+      chart.draw(data, options)
+    })
+}
---
+++ barchart.js
@@ -0,0 +1,26 @@
+
+
+  function drawRegionsMap() {
+    google.charts.load('current', {
+        'packages':['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyDHdelbQAEdfZwnzbCWzu3nQaW2ZO2pBrI'
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
+    var data = google.visualization.arrayToDataTable([
+      ['Country', 'Popularity'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 500],
+      ['France', 600],
+      ['RU', 700]
+    ]);
+
+    var options = {};
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options);
+  }
